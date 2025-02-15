c145921b3f91305a37881a097b44f8fc378c9c1b
ahmadiqballl <comebandage@gmail.com>
2025-02-15 14:23:43 +0700

    Menambahkan file coba.cpp

---
+++ coba.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 10;
+    int *ptr = &num;
+    cout << "NUM | Value = 10 | Addres = 0x81b35ffd1c\n*PTR | Value = add num | Adress 0x81b35ffd10" << endl;
+    cout << "NUM : " << num << endl;
+    cout << "ADD NUM : " << &num << endl;
+    cout << "*PTR : " << *ptr << endl;
+    cout << "PTR : " << ptr << endl;
+    cout << "&PTR : " << &ptr << endl;
+}
